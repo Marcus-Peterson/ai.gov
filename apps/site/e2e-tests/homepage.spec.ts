fbcb49d639cddc2d40bf86cc75971e791cf5cd9b
Ethan Gardner <ethangardner@users.noreply.github.com>
2025-05-28 12:49:13 -0400

    Fix missing skipnav bug (#66)
    * provide default skip link and make it overridable
    
    * add tests for skipnav override
    
    * use empty string instead of an empty div for test slot
    
    * add 1 more test
    
    * add 1 more test

--- apps/site/e2e-tests/homepage.spec.ts
+++ apps/site/e2e-tests/homepage.spec.ts
@@ -3,14 +3,28 @@ import AxeBuilder from '@axe-core/playwright';
 import { HomePage } from "./models/homepage";
 
 test.describe('homepage', () => {
+  let homePage;
+  
+  test.beforeEach(async ({ page }) => {
+    homePage = new HomePage(page);
+    await homePage.navigateTo(`/`);
+  });
+  
   test('should not have any automatically detectable accessibility issues', async ({ page }) => {
-    const homePage = new HomePage(page);
-      await homePage.navigateTo(`/`);
-      
-
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze(); 
 
     expect(accessibilityScanResults.violations).toEqual([]);
+  });
+  
+  test('should contain a skip link as the first focusable item', async ({ page }) => {
+    await page.keyboard.press('Tab');
+    const focusedElement = await page.evaluateHandle(() => document.activeElement);
+    
+    const className = await focusedElement.evaluate((el) =>
+      el.classList.contains('usa-skipnav')
+    );
+    
+    expect(className).toBeTruthy();
   });
   
 });
---
+++ apps/site/src/layouts/BaseLayout.test.ts
@@ -0,0 +1,42 @@
+import { experimental_AstroContainer as AstroContainer } from 'astro/container';
+import { test, expect, describe, beforeEach } from 'vitest';
+// @ts-ignore
+import BaseLayout from './BaseLayout.astro';
+
+describe('BaseLayout', () => {
+  let container: AstroContainer;
+
+  beforeEach(async () => {
+    container = await AstroContainer.create();
+  });
+
+  test('Provides a default value for the skip link', async () => {
+    const result = await container.renderToString(BaseLayout, {
+      slots: {},
+    });
+
+    expect(result).toContain('Skip to main content');
+  });
+
+  test('Can override the skip link', async () => {
+    const link = `<a href="#">Jump to main content</a>`;
+    const result = await container.renderToString(BaseLayout, {
+      slots: {
+        'skip-nav': link,
+      },
+    });
+
+    expect(result).to.contain(link);
+    expect(result).not.toContain('Skip to main content');
+  });
+
+  test('Can remove the skip link', async () => {
+    const result = await container.renderToString(BaseLayout, {
+      slots: {
+        'skip-nav': '',
+      },
+    });
+
+    expect(result).not.toContain('Skip to main content');
+  });
+});
